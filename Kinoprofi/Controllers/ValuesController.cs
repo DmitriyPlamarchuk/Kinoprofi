d535d7a1004d19b2ecd22da20a5a01feaf5b64fb
Dimon <Dimon@DIMONPC>
2018-02-20 14:38:05 +0200

    Changed value 2

--- Kinoprofi/Controllers/ValuesController.cs
+++ Kinoprofi/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace Kinoprofi.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value3" };
+            return new string[] { "value1", "value4" };
         }
 
         // GET api/values/5
